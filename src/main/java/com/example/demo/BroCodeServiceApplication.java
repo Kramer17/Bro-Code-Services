4a8a3838594e87a30ec34e271a82ee8ea46ae54b
Janson Kramer <jjkramer@aep.com>
2019-11-02 12:15:35 -0400

    setup

---
+++ src/main/java/com/example/demo/BroCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroCodeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroCodeServiceApplication.class, args);
+    }
+
+}
